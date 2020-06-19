a2c4544992e26c5f9dc86e6576ea665ee433aa91
ThonyD29 <dayondonthony@gmail.com>
2020-06-19 22:06:50 +0800

    Updates

---
+++ precache-manifest.2bc061a8ffe8752d8ec6290ece988f66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee4f79ec6818bd87a57b6b141b451a3",
+    "url": "/TropaLandingPage/index.html"
+  },
+  {
+    "revision": "7eae24206481e359883f",
+    "url": "/TropaLandingPage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "45cf8fdd7bb55336a0eb",
+    "url": "/TropaLandingPage/static/js/2.d9ebb067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TropaLandingPage/static/js/2.d9ebb067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eae24206481e359883f",
+    "url": "/TropaLandingPage/static/js/main.51c57158.chunk.js"
+  },
+  {
+    "revision": "62622bb622cf36f1d4d1",
+    "url": "/TropaLandingPage/static/js/runtime-main.cd37fc5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TropaLandingPage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3fbaea1ee9b6bbe36aa7997e8a4f2350",
+    "url": "/TropaLandingPage/static/media/pic.3fbaea1e.png"
+  }
+]);
